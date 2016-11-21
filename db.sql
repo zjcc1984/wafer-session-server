501cc02f709621d2d6251cb49bf932161274640e
HNJKSDH <wuliled@163.com>
2016-11-21 15:46:12 +0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,22 +3,27 @@ use cAuth;
 
 DROP TABLE IF EXISTS `cAppinfo`;
 CREATE TABLE `cAppinfo` (
-  `appid` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `appid` varchar(200) COLLATE utf8_unicode_ci NOT NULL ,
   `secret` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
   `login_duration` int(11) DEFAULT '30',
   `session_duration` int(11) DEFAULT '3600',
-   PRIMARY KEY (`appid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `session_duration` int(11) DEFAULT '2592000',
+  `qcloud_appid` varchar(300) COLLATE utf8_unicode_ci DEFAULT 'appid_qcloud',
+  `ip` varchar(50) COLLATE utf8_unicode_ci DEFAULT '0.0.0.0',
+  PRIMARY KEY (`appid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
 DROP TABLE IF EXISTS `cSessioninfo`;
-CREATE TABLE `cSessioninfo` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `skey` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `create_time` int(11) NOT NULL,
-  `last_visit_time` int(11) NOT NULL,
-  `open_id` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `session_key` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `user_info` text COLLATE utf8_unicode_ci,
-   KEY `auth` (`id`,`skey`),
-   KEY `wexin` (`open_id`,`session_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `cSessionInfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `skey` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` datetime NOT NULL,
+  `last_visit_time` datetime NOT NULL,
+  `open_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `session_key` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_info` varchar(2048) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `auth` (`uuid`,`skey`),
+  KEY `weixin` (`open_id`,`session_key`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='会话管理用户信息';
